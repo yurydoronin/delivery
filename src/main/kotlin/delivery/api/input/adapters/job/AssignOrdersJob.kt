ecc1344e673df2c4e1ec9736dde53425e76733d7
Yury Doronin <doronin.ltd@gmail.com>
2025-10-21 23:00:35 +0300

    Модуль 11. Fix - LazyInitializationException

--- src/main/kotlin/delivery/api/input/adapters/job/AssignOrdersJob.kt
+++ src/main/kotlin/delivery/api/input/adapters/job/AssignOrdersJob.kt
@@ -4,11 +4,13 @@ import delivery.core.application.ports.input.commands.OrderAssignmentUseCase
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class AssignOrdersJob(
     private val useCase: OrderAssignmentUseCase
 ) : Job {
+    @Transactional
     override fun execute(context: JobExecutionContext) {
         useCase.execute()
     }
--- src/main/kotlin/delivery/api/input/adapters/job/MoveCouriersJob.kt
+++ src/main/kotlin/delivery/api/input/adapters/job/MoveCouriersJob.kt
@@ -4,11 +4,13 @@ import delivery.core.application.ports.input.commands.CouriersMovementUseCase
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class MoveCouriersJob(
     private val useCase: CouriersMovementUseCase
 ) : Job {
+    @Transactional
     override fun execute(context: JobExecutionContext) {
         useCase.execute()
     }
