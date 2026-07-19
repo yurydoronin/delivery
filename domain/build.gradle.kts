89517ed10d299b07a9e5dfb515b78cabca722bc4
Yury Doronin <doronin.ltd@gmail.com>
2026-07-19 08:57:48 +0300

    Refactor: domain build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 dependencies {
     implementation(project(":common:types"))
 
-    implementation("com.github.f4b6a3:uuid-creator:6.1.1")
+    implementation(libs.uuid.creator)
 
     // Arrow.Either
-    implementation("io.arrow-kt:arrow-core:2.2.2.1")
+    implementation(libs.arrow.core)
 }
